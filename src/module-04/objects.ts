576e56c6e988c5bd875046f069bbfd44174d6e41
yakubsiyam <itsyakubsiyam@gmail.com>
2023-05-05 17:07:35 +0600

    create object and class

---
+++ src/module-04/objects.ts
@@ -0,0 +1,36 @@
+// class Animal {
+//     name: string;
+//     species: string;
+//     sound: string;
+
+//   constructor(
+//     name: string,
+//     species: string,
+//     sound: string
+//   ) {
+//     this.name = name;
+//     this.species = species;
+//     this.sound = sound;
+//   }
+
+//   public makeSound() {
+//     console.log(`The ${this.name} says ${this.sound}`);
+//   }
+// }
+
+// class Animal {
+//   constructor(
+//     public name: string,
+//     public species: string,
+//     public sound: string
+//   ) {}
+
+//   public makeSound() {
+//     console.log(`The ${this.name} says ${this.sound}`);
+//   }
+// }
+
+// const dog = new Animal("German Shepard", "dog", "Ghew Ghew");
+// const cat = new Animal("Persian", "cat", "Meaw meaw");
+// dog.makeSound();
+// cat.makeSound();
